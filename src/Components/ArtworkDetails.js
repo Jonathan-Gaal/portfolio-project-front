63b85d0f8191d2e500e8dec53d69b91a39b0e6f1
Jonathan-Gaal <jonathangaal@pursuit.org>
2023-08-07 16:27:44 -0400

    added transform and rotate on hover to artworkCard

--- src/Components/ArtworkDetails.js
+++ src/Components/ArtworkDetails.js
@@ -45,22 +45,19 @@ const ArtworkDetails = () => {
     <div className="Artwork">
       <img id="artworkImage" src={image} alt="artwork"></img>
       <h1>{title}</h1>
-      <h3>Category: {category}</h3>
-
-      <h3>Description: {description}</h3>
-      <h3>Date Posted: {post_date}</h3>
-
+      <h3>Category: {category}</h3>{" "}
       <h3>
         Dimensions: Width:{width} x Height:{height} x Depth:{depth} Diameter:
         {diameter}
       </h3>
-
+      <h3>Date Posted: {post_date}</h3>
+      <h3>Description: {description}</h3>
       <div className="ShowPageButtons">
         <Link to={`/gallery`}>
-          <button id="galleryBtn">Back to Gallery</button>
+          <button id="galleryBtn">Gallery</button>
         </Link>
         <button id="commentBtn" onClick={displayComments}>
-          Please Comment
+          Comment
         </button>
       </div>
       <Comments showComments={showComments} />
