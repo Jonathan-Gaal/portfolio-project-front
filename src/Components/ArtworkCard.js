efe2501ed89632d944eff8478361a6afc3245ffa
Jonathan-Gaal <jonathangaal@pursuit.org>
2023-09-13 16:46:33 -0400

    all images are fetched from images table

--- src/Components/ArtworkCard.js
+++ src/Components/ArtworkCard.js
@@ -1,14 +1,36 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
 import { Link } from "react-router-dom";
 import "./ArtworkCard.scss";
+const API = process.env.REACT_APP_API_URL;
 
 // ARTWORK DISPLAYED IN gallery.map in GALLERY.js
 const ArtworkCard = ({ artwork }) => {
-  const { title, category, image } = artwork;
+  const { id, title, category } = artwork;
+
+  const [artworkCardMainImage, setArtworkCardMainImage] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`${API}/gallery/${id}/artImages`, {
+        headers: { "Access-Control-Allow-Origin": "*" },
+      })
+      .then((res) => {
+        setArtworkCardMainImage(res.data[0].image_url);
+      })
+      .catch((err) => {
+        console.warn("catch", err);
+      });
+  }, [id]);
 
   return (
     <Link to={`/gallery/${artwork.id}`}>
       <div className="ArtworkCard">
-        <img className="galleryImage" src={image} alt="artwork" />
+        <img
+          className="galleryImage"
+          src={artworkCardMainImage}
+          alt="artwork"
+        />
         <div className="artWorkCard__details">
           <div>{title}</div>
           <div>Category: {category}</div>
