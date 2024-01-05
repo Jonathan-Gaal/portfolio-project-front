7e36c0a63e9a6557191a3e2ffbccdef3bf46460f
Jonathan-Gaal <jonathangaal@pursuit.org>
2024-01-04 19:26:53 -0500

    navbar converted to scss

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,4 +1,4 @@
-import { Link, Navigate, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import navBarLogo from "../assets/navBar_logo.jpg";
 import { useAuth } from "../Providers/userProvider";
 import "./NavBar.scss";
@@ -16,36 +16,41 @@ const NavBar = () => {
 
   return (
     <div className="NavBar">
-      <div className="navBarHeader">
-        <img className="navBarLogo" src={navBarLogo} alt="arabic teapot" />
-        <Link to="/">
-          <div className="navBarHeaderText"> Jon's Palace of Art</div>
-        </Link>
+      <div className="NavBar__header">
+        <img className="NavBar__logo" src={navBarLogo} alt="arabic teapot" />
+
+        <div className="NavBar__headerText" onClick={() => navigate("/")}>
+          Jon's Palace Of Art
+        </div>
       </div>
 
-      <div className="navBarButtons">
+      <div className="NavBar__navigationButtons">
         <button className="seeGalleryBtn" onClick={() => navigate("/gallery")}>
           Gallerie
         </button>
 
         {loggedInUser ? (
           <button
-            className="dashboardBtn"
+            className="NavBar__dashboardBtn"
             onClick={() => navigate("/dashboard")}>
             Dashboard
           </button>
         ) : null}
 
-        <button className="signUpBtn" onClick={() => navigate("/signup")}>
+        <button
+          className="NavBar__signUpBtn"
+          onClick={() => navigate("/signup")}>
           Sign Up
         </button>
 
         {loggedInUser ? (
-          <button className="signOutBtn" onClick={signOutButton}>
+          <button className="NavBar__signOutBtn" onClick={signOutButton}>
             Sign Out
           </button>
         ) : (
-          <button className="signInBtn" onClick={() => navigate("/signin")}>
+          <button
+            className="NavBar__signInBtn"
+            onClick={() => navigate("/signin")}>
             Sign In
           </button>
         )}
