0c322cebe1506f013bd9bfd4f54c3c1e2aa12488
Jonathan-Gaal <jonathangaal@pursuit.org>
2024-01-09 22:37:53 -0500

    added .js to favorites in app.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { SignIn } from "./Pages/SignIn";
 import { SignUp } from "./Pages/SignUp";
 import { Dashboard } from "./Pages/Dashboard";
 import { Account } from "./Pages/Account";
-import { UserFavorites } from "./Components/UserComponents/UserFavorites";
+import { UserFavorites } from "./Components/UserComponents/UserFavorites.js";
 import { Cart } from "./Pages/Cart";
 import { PaymentSuccess } from "./Pages/PaymentSuccess";
 import { PaymentFailed } from "./Pages/PaymentFailed";
